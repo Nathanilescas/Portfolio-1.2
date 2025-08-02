d1fdf9b24fc4d20355a5295aec8ea9cc7917c47e
DnuevoDesign <nathan.ilescas1@gmail.com>
2025-08-01 18:11:29 -0700

    Starting over

--- Client/Data/Tree-DS-Images/term-definitions.js
+++
@@ -1,9 +0,0 @@
-export default definitions = {
-    'root' : "The topmost node in a tree data structure. This node has no parent node above it, meaning it is the starting point of the tree",
-    'edge' :  "The connection or link between two nodes in a tree data structure.",
-    'siblings': "These are all the child nodes that share the same parent node.",
-    'leaf' : "The bottommost nodes in a tree that do not have any children. They are the end points of the tree.",
-    'parent' : "A node that has one or more child nodes directly connected to it.",
-    'child' : "A node that comes directly after and is connected to a parent node in the tree.",
-    'sub-tree' : "A part of a tree that consists of a parent node and its direct child nodes."
-}
--- Client/src/Projects/Beta-Scripts/src/Server/StoreageSystem/userType.tsx
+++
@@ -1,5 +0,0 @@
-export enum UserType {
-    UNDEFINED = 0,
-    HOST = 1,
-    CLIENT = 2,
-}
